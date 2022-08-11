204c274ce0d3300db1ace676758e670b75a3b7d9
Eugenio Contreras <ecs.keni@gmail.com>
2022-08-11 19:00:45 -0400

    Fix issue with saveLoop and gob: duplicate type received

--- store.go
+++ store.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	//	"bufio"
 	"encoding/gob"
-	"fmt"
 	"io"
+	"log"
 	"os"
 	"sync"
 )
@@ -25,26 +26,22 @@ func NewURLStore(filename string) *URLStore {
 		urls: make(map[string]string),
 		save: make(chan record, saveQueueLength),
 	}
-
 	if err := s.load(filename); err != nil {
-		fmt.Errorf("error loading %s: %v", filename, err)
+		log.Printf("error loading %s: %v", filename, err)
 	}
-
 	go s.saveLoop(filename)
 	return s
-
 }
 
 func (s *URLStore) Get(key string) string {
-	s.mu.RLock() // multiple go routines can read at a time but only one go routine can write at a time
+	s.mu.RLock()
 	defer s.mu.RUnlock()
 	return s.urls[key]
 }
 
 func (s *URLStore) Set(key, url string) bool {
-	s.mu.Lock() // only one go routine can write at a time
+	s.mu.Lock()
 	defer s.mu.Unlock()
-
 	if _, present := s.urls[key]; present {
 		return false
 	}
@@ -60,9 +57,9 @@ func (s *URLStore) Count() int {
 
 func (s *URLStore) Put(url string) string {
 	for {
-		key := genKey(s.Count()) // generate short url
-		if s.Set(key, url) {
-			s.save <- record{key, url} // save to go routine
+		key := genKey(s.Count())
+		if ok := s.Set(key, url); ok {
+			s.save <- record{key, url}
 			return key
 		}
 	}
@@ -70,17 +67,12 @@ func (s *URLStore) Put(url string) string {
 }
 
 func (s *URLStore) load(filename string) error {
-
 	f, err := os.Open(filename)
 	if err != nil {
-		PrintHandleError(err)
 		return err
 	}
-
 	defer f.Close()
-
 	d := gob.NewDecoder(f)
-
 	for err == nil {
 		var r record
 		if err = d.Decode(&r); err == nil {
@@ -97,13 +89,15 @@ func (s *URLStore) load(filename string) error {
 
 func (s *URLStore) saveLoop(filename string) {
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	FatalHandleError(err)
-
-	defer f.Close()
-	e := gob.NewEncoder(f) // return a encoder to io.Writer
-	for {
-		r := <-s.save // taking a record from the channel and encoding it to the file
-		PrintHandleError(e.Encode(r))
+	if err != nil {
+		log.Fatal("Error opening URLStore: ", err)
 	}
-
+	defer f.Close()
+	e := gob.NewEncoder(f)
+	for {
+		r := <-s.save // takes a record from the channel
+		if err := e.Encode(r); err != nil {
+			log.Println("Error saving to URLStore: ", err)
+		}
+	}
 }
