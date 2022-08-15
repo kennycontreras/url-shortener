65100f99b8938f4a8247cc3e445923cb2763ac80
Eugenio Contreras <ecs.keni@gmail.com>
2022-08-14 20:39:03 -0400

    Add implementation for multiprocessing with RPC

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"net/rpc"
 )
 
 const addForm = `
@@ -15,17 +16,31 @@ URL: <input type="text" name="url">
 <\html><\body>
 `
 
-var store *URLStore
+var store Store
 
 var (
 	listenAddr = flag.String("http", ":8080", "HTTP listen address")
 	dataFile   = flag.String("file", "store.json", "json store filename")
+	rpcEnabled = flag.Bool("rpc", false, "enable RPC server")
 	hostname   = flag.String("hostname", "localhost", "hostname and port")
+	masterAddr = flag.String("master", "", "RPC master address")
 )
 
 func main() {
 	flag.Parse()
 	store = NewURLStore(*dataFile)
+
+	if *masterAddr != "" {
+		store = NewProxyStore(*masterAddr)
+	} else {
+		store = NewURLStore(*dataFile)
+	}
+
+	if *rpcEnabled {
+		rpc.RegisterName("Store", store)
+		rpc.HandleHTTP()
+	}
+
 	http.HandleFunc("/", Redirect)
 	http.HandleFunc("/add", Add)
 	err := http.ListenAndServe(*listenAddr, nil)
@@ -39,17 +54,25 @@ func Add(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, addForm)
 		return
 	}
-	key := store.Put(url)
+	var key string
+	if err := store.Put(&url, &key); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 
 	fmt.Fprintf(w, "Added url %s as %s", url, key)
 }
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
 	key := r.URL.Path[1:]
-	url := store.Get(key)
-	if url == "" {
+	if key == "" {
 		http.NotFound(w, r)
 		return
 	}
+	var url string
+	if err := store.Get(&key, &url); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	http.Redirect(w, r, url, http.StatusFound)
 }
--- store.go
+++ store.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"log"
+	"net/rpc"
 	"os"
 	"sync"
 )
 
 const saveQueueLength = 1000
+
+type Store interface {
+	Put(url, key *string) error
+	Get(key, url *string) error
+}
 
 type URLStore struct {
 	urls map[string]string
@@ -20,32 +27,42 @@ type record struct {
 	Key, URL string
 }
 
+type ProxyStore struct {
+	client *rpc.Client
+	urls   *URLStore
+}
+
 func NewURLStore(filename string) *URLStore {
-	s := &URLStore{
-		urls: make(map[string]string),
-		save: make(chan record, saveQueueLength),
+	s := &URLStore{urls: make(map[string]string)}
+	if filename != "" {
+		s.save = make(chan record, saveQueueLength)
+		if err := s.load(filename); err != nil {
+			log.Println("Error loading URLStore: ", err)
+		}
+		go s.saveLoop(filename)
 	}
-	if err := s.load(filename); err != nil {
-		log.Printf("error loading %s: %v", filename, err)
-	}
-	go s.saveLoop(filename)
+
 	return s
 }
 
-func (s *URLStore) Get(key string) string {
+func (s *URLStore) Get(key, url *string) error {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-	return s.urls[key]
+	if u, present := s.urls[*key]; present {
+		*url = u
+		return nil
+	}
+	return errors.New("key not found")
 }
 
-func (s *URLStore) Set(key, url string) bool {
+func (s *URLStore) Set(key, url *string) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	if _, present := s.urls[key]; present {
-		return false
+	if _, present := s.urls[*key]; present {
+		return errors.New("key already present")
 	}
-	s.urls[key] = url
-	return true
+	s.urls[*key] = *url
+	return nil
 }
 
 func (s *URLStore) Count() int {
@@ -54,15 +71,18 @@ func (s *URLStore) Count() int {
 	return len(s.urls)
 }
 
-func (s *URLStore) Put(url string) string {
+func (s *URLStore) Put(url, key *string) error {
 	for {
-		key := genKey(s.Count())
-		if ok := s.Set(key, url); ok {
-			s.save <- record{key, url}
-			return key
+		*key = genKey(s.Count())
+		if err := s.Set(key, url); err != nil {
+			break
 		}
 	}
-	panic("unreachable")
+
+	if s.save != nil {
+		s.save <- record{*key, *url}
+	}
+	return nil
 }
 
 func (s *URLStore) load(filename string) error {
@@ -75,7 +95,7 @@ func (s *URLStore) load(filename string) error {
 	for err == nil {
 		var r record
 		if err = d.Decode(&r); err == nil {
-			s.Set(r.Key, r.URL)
+			s.Set(&r.Key, &r.URL)
 		}
 	}
 	if err == io.EOF {
@@ -99,4 +119,33 @@ func (s *URLStore) saveLoop(filename string) {
 			log.Println("Error saving to URLStore: ", err)
 		}
 	}
+}
+
+func NewProxyStore(addr string) *ProxyStore {
+	client, err := rpc.Dial("tcp", addr)
+	if err != nil {
+		log.Println("Error dialing: ", err)
+	}
+	return &ProxyStore{urls: NewURLStore(""), client: client}
+}
+
+func (p *ProxyStore) Get(key, url *string) error {
+	if err := p.urls.Get(key, url); err == nil { // check local cache
+		return nil
+	}
+
+	if err := p.client.Call("Store.Get", key, url); err != nil {
+		return err
+	}
+
+	p.urls.Set(key, url)
+	return nil
+}
+
+func (p *ProxyStore) Put(key, url *string) error {
+	if err := p.client.Call("Store.Put", key, url); err != nil {
+		return err
+	}
+	p.urls.Set(key, url) // update local cache
+	return nil
 }
