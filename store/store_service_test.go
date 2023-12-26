f6feae6accd7bcc523f5ef30a9b522bb54977915
Eugenio Contreras <ecs.keni@gmail.com>
2023-12-26 18:14:21 +0100

    Add userId as constant

--- store/store_service_test.go
+++ store/store_service_test.go
@@ -6,6 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	userUUID = "90a1d2b1-f480-4f90-9aba-795a19273e58"
+)
+
 var testStoreService = &StorageService{}
 
 func init() {
@@ -18,7 +22,6 @@ func TestStoreInit(t *testing.T) {
 
 func TestInsertionAndRetrieval(t *testing.T) {
 	initialUrl := "https://www.mydealz.de/deals"
-	userUUID := "90a1d2b1-f480-4f90-9aba-795a19273e58"
 	shortUrl := "f480"
 
 	SaveUrlMapping(shortUrl, initialUrl, userUUID)
